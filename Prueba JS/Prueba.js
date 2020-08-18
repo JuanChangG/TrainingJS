873e91feb481b3eb06abf8574deac22916e59c7c
Juan Chang <jchang2406@gmail.com>
2020-08-18 08:11:15 -0500

    Training

---
+++ Prueba JS/Prueba.js
@@ -0,0 +1,16 @@
+
+   var diaSemana = new Array(7); // Creamos un array con 7 posiciones
+   diaSemana[0] = "lunes";
+   diaSemana[1] = "martes";
+   diaSemana[2] = "miércoles";
+   diaSemana[3] = "jueves";
+   diaSemana[4] = "viernes";
+   diaSemana[5] = "sábado";
+   diaSemana[6] = "domingo";
+
+   for (var i=0; i <= diaSemana.length -1; i++)
+   {
+       console.log(diaSemana[i]);
+   }
+
+
